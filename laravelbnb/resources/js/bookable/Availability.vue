bccbe49795890cb1b5c4808f6876542748430e92
Luc Guimbertaud <luc.guimbertaud@orange.fr>
2020-05-05 08:58:53 +0200

    Get datas from the form in front

--- laravelbnb/resources/js/bookable/Availability.vue
+++ laravelbnb/resources/js/bookable/Availability.vue
@@ -10,6 +10,8 @@
                     name="from"
                     class="form-control form-control-sm"
                     placeholder="Start date"
+                    v-model="from"
+                    @keyup.enter="check"
                 >
             </div>
 
@@ -20,12 +22,34 @@
                     name="to"
                     class="form-control form-control-sm"
                     placeholder="End date"
+                    v-model="to"
+                    @keyup.enter="check"
                 >
             </div>
         </div>
-        <button class="btn btn-secondary btn-block">Check</button>
+        <button class="btn btn-secondary btn-block" @click="check">Check</button>
     </div>
 </template>
+
+
+
+<script>
+export default {
+    data(){
+        return{
+            from: null,
+            to: null,
+        }
+    },
+
+    methods: {
+        check(){
+            alert('I will check something now')
+        }
+    },
+}
+</script>
+
 
 
 
