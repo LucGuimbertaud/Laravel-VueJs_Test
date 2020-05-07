5a9d1bad346a5c2c1a84a13441e5d4391da9faea
Luc Guimbertaud <luc.guimbertaud@orange.fr>
2020-05-07 15:29:44 +0200

    Change the way to display the date of reviews with moment.js

--- laravelbnb/resources/js/bookable/ReviewList.vue
+++ laravelbnb/resources/js/bookable/ReviewList.vue
@@ -2,7 +2,7 @@
     <div style="padding: 1.25rem">
         <h6 class="text-uppercase text-secondary font-weight-bolder pt-4">Review List</h6>
 
-        <div v-if="loadin">Loading...</div>
+        <div v-if="loading">Loading...</div>
         <div v-else>
             <div class="border-bottom d-none d-md-block" v-for="(review, index) in reviews" :key="index">
                 <div class="row pt-4">
@@ -10,7 +10,7 @@
                     <div class="col-md-6 d-flex justify-content-end">{{ review.rating }}</div>
                 </div>
                 <div class="row">
-                    <div class="col-md-12">{{ review.created_at }}</div>
+                    <div class="col-md-12">{{ review.created_at | fromNow }}</div>
                 </div>
                 <div class="row pt-4 pb-4">
                     <div class="col-md-12">{{ review.content }}</div>
@@ -22,6 +22,8 @@
 
 
 <script>
+// import moment from "moment"
+
 export default {
     props: {
         bookableId: String
@@ -31,13 +33,19 @@ export default {
         return{
             loading: false,
             reviews: null
-        }
+        };
     },
     created(){
         this.loading = true;
         axios.get(`/api/bookables/${this.bookableId}/reviews`)
             .then(response => this.reviews = response.data.data)
             .then(() => this.loading = false);
-    }
+    },
+
+    // filters: {
+    //     fromNow(value) {
+    //         return moment(value).fromNow();
+    //     }
+    // }
 }
 </script>
