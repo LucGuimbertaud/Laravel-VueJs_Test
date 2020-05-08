61c31df5ddf090a690761f00f7498d9bdef6d568
Luc Guimbertaud <luc.guimbertaud@orange.fr>
2020-05-08 09:41:19 +0200

    Fix error of returning the wrong type of reviews

--- laravelbnb/public/js/app.js
+++ laravelbnb/public/js/app.js
@@ -2259,7 +2259,7 @@ __webpack_require__.r(__webpack_exports__);
 //
 /* harmony default export */ __webpack_exports__["default"] = ({
   props: {
-    rating: Number
+    rating: null
   },
   computed: {
     halfStar: function halfStar() {
--- laravelbnb/resources/js/shared/components/StarRating.vue
+++ laravelbnb/resources/js/shared/components/StarRating.vue
@@ -9,7 +9,7 @@
 <script>
 export default {
     props: {
-        rating: Number
+        rating: null
     },
 
     computed: {
