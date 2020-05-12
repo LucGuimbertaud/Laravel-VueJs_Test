7407a6d67876b286381de73f5b5c1ae69cc86984
Luc Guimbertaud <luc.guimbertaud@orange.fr>
2020-05-12 22:44:19 +0200

    Add Vuex in app

--- laravelbnb/resources/js/app.js
+++ laravelbnb/resources/js/app.js
@@ -1,5 +1,6 @@
 require("./bootstrap");
 
+import Vuex from "vuex"
 import moment from "moment";
 import VueRouter from "vue-router";
 import Index from "./Index";
@@ -8,9 +9,11 @@ import FatalError from "./shared/components/FatalError";
 import Success from "./shared/components//Success";
 import StarRating from "./shared/components/StarRating";
 import ValidationErrors from "./shared/components/ValidationErrors";
+import storeDefinition from "./store";
 
 window.Vue = require("vue");
 Vue.use(VueRouter);
+Vue.use(Vuex);
 
 Vue.filter("fromNow", value => moment(value).fromNow());
 
@@ -19,10 +22,12 @@ Vue.component("fatal-error", FatalError);
 Vue.component("v-errors", ValidationErrors);
 Vue.component("success", Success);
 
+const store = new Vuex.Store(storeDefinition);
 
 const app = new Vue({
     el: "#app",
     router,
+    store,
     components: {
         index: Index
     }
