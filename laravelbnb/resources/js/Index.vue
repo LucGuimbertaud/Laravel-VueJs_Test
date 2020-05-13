c493851211d88eab055cb7f64d9386a9740ad780
Luc Guimbertaud <luc.guimbertaud@orange.fr>
2020-05-13 12:19:19 +0200

    Use Vuex actions to store object to local storage

--- laravelbnb/resources/js/Index.vue
+++ laravelbnb/resources/js/Index.vue
@@ -10,3 +10,28 @@
 
     </div>
 </template>
+
+
+
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+    data() {
+        return {
+            lastSearch: this.$store.lastSearch
+        }
+    },
+
+    computed: {
+        ...mapState({
+        lastSearchComputed: "lastSearch"
+    }),
+        somethingElse() {
+            return 1+2;
+        }
+    }
+
+}
+</script>
